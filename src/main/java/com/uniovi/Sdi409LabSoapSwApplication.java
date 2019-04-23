04913c25ad5296e09366eda204279d7663cb3798
Unknown <mor-nia@hotmail.com>
2019-04-23 21:52:56 +0200

    SDI - SW - 11.1 Servicio SOAP

---
+++ src/main/java/com/uniovi/Sdi409LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi409LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi409LabSoapSwApplication.class, args);
+	}
+
+}
